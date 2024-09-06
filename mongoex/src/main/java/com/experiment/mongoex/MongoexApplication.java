2de1a91bbf5a6ad2311567b2b07dfb4190ba2d16
LahiruCW <as2021356@sci.sjp.ac.lk>
2024-09-06 21:57:51 +0530

    added another learning

---
+++ mongoex/src/main/java/com/experiment/mongoex/MongoexApplication.java
@@ -0,0 +1,13 @@
+package com.experiment.mongoex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoexApplication.class, args);
+	}
+
+}
